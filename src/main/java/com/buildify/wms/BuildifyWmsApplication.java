012acbfd42801327e28f668dd77ee7860e0beef6
minhyeokshin <sinminhyeok@gmail.com>
2025-04-04 11:22:08 +0900

    🎉 Init: set up initial project

---
+++ src/main/java/com/buildify/wms/BuildifyWmsApplication.java
@@ -0,0 +1,13 @@
+package com.buildify.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildifyWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildifyWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildify/wms/BuildifyWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildify.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildifyWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
