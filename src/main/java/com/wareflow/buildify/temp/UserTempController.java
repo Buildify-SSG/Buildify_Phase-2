25fdea67274ea8e5fb97e88d2429048cab70e45a
minhyeokshin <sinminhyeok@gmail.com>
2025-04-08 09:36:24 +0900

    ✨ feature : 프론트 페이지 레이아웃 구조 설계 및 작성

---
+++ src/main/java/com/wareflow/buildify/temp/UserTempController.java
@@ -0,0 +1,97 @@
+package com.wareflow.buildify.temp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserTempController {
+
+    @GetMapping("/pages/index")
+    public String index() {
+        return "users/pages/index";
+    }
+
+    @GetMapping("/pages/inbound/inbound-1")
+    public String inbound1() {
+        return "users/pages/inbound/inbound-1";
+    }
+
+    @GetMapping("/pages/inbound/inbound-2")
+    public String inbound2() {
+        return "users/pages/inbound/inbound-2";
+    }
+
+    @GetMapping("/pages/inbound/inbound-3")
+    public String inbound3() {
+        return "users/pages/inbound/inbound-3";
+    }
+
+    @GetMapping("/pages/inbound/inbound-4")
+    public String inbound4() {
+        return "users/pages/inbound/inbound-4";
+    }
+
+
+    @GetMapping("/pages/inventory/inventory-1")
+    public String inventory1() {
+        return "users/pages/inventory/inventory-1";
+    }
+
+    @GetMapping("/pages/inventory/inventory-2")
+    public String inventory2() {
+        return "users/pages/inventory/inventory-2";
+    }
+
+    @GetMapping("/pages/inventory/inventory-3")
+    public String inventory3() {
+        return "users/pages/inventory/inventory-3";
+    }
+
+    @GetMapping("/pages/inventory/inventory-4")
+    public String inventory4() {
+        return "users/pages/inventory/inventory-4";
+    }
+
+    @GetMapping("/pages/outbound/outbound-1")
+    public String outbound1() {
+        return "users/pages/outbound/outbound-1";
+    }
+
+    @GetMapping("/pages/outbound/outbound-2")
+    public String outbound2() {
+        return "users/pages/outbound/outbound-2";
+    }
+
+    @GetMapping("/pages/outbound/outbound-3")
+    public String outbound3() {
+        return "users/pages/outbound/outbound-3";
+    }
+
+    @GetMapping("/pages/outbound/outbound-4")
+    public String outbound4() {
+        return "users/pages/outbound/outbound-4";
+    }
+
+    @GetMapping("/pages/product/product-1")
+    public String product1() {
+        return "users/pages/product/product-1";
+    }
+
+    @GetMapping("/pages/product/product-2")
+    public String product2() {
+        return "users/pages/product/product-2";
+    }
+
+    @GetMapping("/pages/product/product-3")
+    public String product3() {
+        return "users/pages/product/product-3";
+    }
+
+    @GetMapping("/pages/product/product-4")
+    public String product4() {
+        return "users/pages/product/product-4";
+    }
+
+}
