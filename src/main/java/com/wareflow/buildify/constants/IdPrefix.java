4fe7e2215827d4d96955d225492ba70b12fb04da
donghwi <cksael@gmail.com>
2025-04-15 11:18:24 +0900

    feature : 공통된 UUID 패턴 적용하기 위한 UUID 모듈 생성

---
+++ src/main/java/com/wareflow/buildify/constants/IdPrefix.java
@@ -0,0 +1,11 @@
+package com.wareflow.buildify.constants;
+
+public enum IdPrefix {
+    USR, // 고객
+    ADM, // 관리자
+    OUT, // 출고
+    INN, // 입고
+    WRH, // 창고
+    PRD, // 상품
+    CAT  // 카테고리
+}
