87d6904dc4d30f1643da63e98562843012d00848
donghwi <cksael@gmail.com>
2025-04-16 14:36:21 +0900

    wip - 회원가입 기능 구현 중
    bug - IndexView 에서 이미 getmapping 처리되어있어서 컨트롤러와 충돌
    
    fix - root-context.xml에서 경로 이슈로 mapper 스캔이 안됨 (아래와 같이 수정)
    
        <mybatis:scan base-package="
        com.wareflow.buildify.domain.auth.signup.mapper,
        com.wareflow.buildify.domain.auth.login.mapper,
        com.wareflow.buildify.domain.admin.inbound.mapper,
        com.wareflow.buildify.domain.admin.inventory.mapper,
        com.wareflow.buildify.domain.admin.outbound.mapper,
        com.wareflow.buildify.domain.admin.product.mapper,
        com.wareflow.buildify.domain.admin.systemOperation.mapper,
        com.wareflow.buildify.domain.admin.userManagement.mapper,
        com.wareflow.buildify.domain.user.product.mapper,
        com.wareflow.buildify.domain.user.outbound.mapper,
        com.wareflow.buildify.domain.user.inbound.mapper,
        com.wareflow.buildify.domain.user.inventory.mapper
        "/>

--- src/main/java/com/wareflow/buildify/domain/auth/signup/service/SignupServiceImpl.java
+++ src/main/java/com/wareflow/buildify/domain/auth/signup/service/SignupServiceImpl.java
@@ -1,4 +1,47 @@
 package com.wareflow.buildify.domain.auth.signup.service;
 
+import com.wareflow.buildify.constants.IdPrefix;
+import com.wareflow.buildify.domain.auth.signup.mapper.SignupMapper;
+import com.wareflow.buildify.dto.UserDTO;
+import com.wareflow.buildify.util.CustomIdGenerator;
+import com.wareflow.buildify.vo.UserVO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
 public class SignupServiceImpl implements SignupService {
+    private final SignupMapper signupMapper;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public boolean register(UserDTO userDTO) {
+        // 아이디 중복 검사
+        if (signupMapper.existCheckId(userDTO.getUserId()) > 0) {
+            return false;
+        }
+
+        // 기본값 세팅: client_id, 가입일자, 상태값
+        UserVO userVO = modelMapper.map(userDTO, UserVO.class);
+
+        // 서버에서 세팅해야 하는 필드들
+        userVO = UserVO.builder().clientId(
+                (CustomIdGenerator.generateId(IdPrefix.USR)))
+                .userName(userVO.getUserName())
+                .userPhone(userVO.getUserPhone())
+                .userEmail(userVO.getUserEmail())
+                .userAddress(userVO.getUserAddress())
+                .businessNumber(userVO.getBusinessNumber())
+                .userId(userVO.getUserId())
+                .userPw(userVO.getUserPw()) // 암호화 생략 상태
+                .userEnterdate(new java.util.Date())
+                .userStatus(0) // 미승인 상태
+                .build();
+
+        return signupMapper.insert(userVO) == 1;
+    }
+
+
 }
--- src/main/java/com/wareflow/buildify/vo/UserVO.java
+++ src/main/java/com/wareflow/buildify/vo/UserVO.java
@@ -1,4 +1,26 @@
 package com.wareflow.buildify.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserVO {
+    private String clientId;
+    private String userName;
+    private String userPhone;
+    private String userEmail;
+    private String userAddress;
+    private String businessNumber;
+    private Date userEnterdate;
+    private String userId;
+    private String userPw;
+    private int userStatus;
 }
