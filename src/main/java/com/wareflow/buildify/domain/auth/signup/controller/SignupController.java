d806f7a46893ea5226323b1481a22a8bcf03f403
donghwi <cksael@gmail.com>
2025-04-22 10:35:40 +0900

    fix : 창고 신청 , 회원가입 오류 수정

--- src/main/java/com/wareflow/buildify/domain/auth/signup/controller/SignupController.java
+++ src/main/java/com/wareflow/buildify/domain/auth/signup/controller/SignupController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Log4j2
 public class SignupController {
 
-    private SignupService signupService;
+    private final SignupService signupService;
 
     @GetMapping("/signup")
     public String signup() {
