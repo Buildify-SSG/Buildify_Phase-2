1f0add5d686acb3518ca7003aa8c122192400fcd
seongjun <kimsj0118@gmail.com>
2025-04-24 14:01:31 +0900

    🔧 작업 내용 인바운드 관리자 입고요청 승인구현

--- src/main/java/com/wareflow/buildify/vo/ProductVO.java
+++ src/main/java/com/wareflow/buildify/vo/ProductVO.java
@@ -1,9 +1,6 @@
 package com.wareflow.buildify.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -11,6 +8,7 @@ import java.math.BigDecimal;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class ProductVO {
     private String prodId;         // [접두어]-[날짜]-[랜덤문자열]
     private String brand;
