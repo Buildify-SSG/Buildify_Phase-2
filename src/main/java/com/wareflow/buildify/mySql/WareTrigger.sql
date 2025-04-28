3807c2af40897738db3c3bc9c0bba1311a1205ab
minhyeokshin <sinminhyeok@gmail.com>
2025-04-28 12:37:39 +0900

    🐛 fix : 트리거 로직 및 창고 사용률 계산 로직 수정

--- src/main/java/com/wareflow/buildify/mySql/WareTrigger.sql
+++ src/main/java/com/wareflow/buildify/mySql/WareTrigger.sql
@@ -1,44 +1,35 @@
 # 창고 면적 계산 트리거
-# userWareHouse테이블 신규 계약시 warehouse_area 테이블 업데이트
-
-DELIMITER //
-
-CREATE TRIGGER trg_update_available_space_userwarehouse_insert
-    AFTER INSERT ON userWareHouse
-    FOR EACH ROW
-BEGIN
-    UPDATE warehouse_area
-    SET available_space = GREATEST(
-            (SELECT ware_total_size FROM warehouse_area WHERE ware_id = NEW.ware_id)
-                - (SELECT IFNULL(SUM(warehouse_usage), 0) FROM userWareHouse WHERE ware_id = NEW.ware_id),
-            0
-                          )
-    WHERE ware_id = NEW.ware_id;
-END //
-
-DELIMITER ;
-
-
-# 창고 면적 계산 트리거
 # userWareHouse테이블 업데이트시 warehouse_area 테이블 업데이트
 
 
-
 DELIMITER //
 
+-- AFTER UPDATE 트리거
 CREATE TRIGGER trg_update_available_space_userwarehouse_update
     AFTER UPDATE ON userWareHouse
     FOR EACH ROW
 BEGIN
+    DECLARE total_usage DECIMAL(10, 2);
+    DECLARE total_size DECIMAL(10, 2);
+
+    -- userWareHouse로부터 해당 창고의 전체 사용량 계산
+    SELECT IFNULL(SUM(warehouse_usage), 0)
+    INTO total_usage
+    FROM userWareHouse
+    WHERE ware_id = NEW.ware_id;
+
+    -- warehouse_area의 총 크기 조회
+    SELECT ware_total_size
+    INTO total_size
+    FROM warehouse_area
+    WHERE ware_id = NEW.ware_id;
+
+    -- available_space 업데이트
     UPDATE warehouse_area
-    SET available_space = GREATEST(
-            (SELECT ware_total_size FROM warehouse_area WHERE ware_id = NEW.ware_id)
-                - (SELECT IFNULL(SUM(warehouse_usage), 0) FROM userWareHouse WHERE ware_id = NEW.ware_id),
-            0
-                          )
+    SET available_space = GREATEST(total_size - total_usage, 0)
     WHERE ware_id = NEW.ware_id;
 END //
 
 DELIMITER ;
 
-SHOW TRIGGERS;
+SHOW TRIGGERS
